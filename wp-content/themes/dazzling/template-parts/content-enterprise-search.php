85dd83a26aa25fa9b0540e4767ba18fb02adf174
Justin MacNeil <justin.macneil@bv02.com>
2016-06-25 16:08:17 -0400

    search/filtering

--- wp-content/themes/dazzling/template-parts/content-enterprise-search.php
+++ wp-content/themes/dazzling/template-parts/content-enterprise-search.php
@@ -30,46 +30,81 @@
 		
 		$cluster  = isset($_GET['cluster']) ? $_GET['cluster'] : '';
 		$segments = isset($_GET['segments']) ? $_GET['segments'] : '';
+		$keyword  = isset($_GET['keyword']) ? sanitize_text_field($_GET['keyword']) : '';
+		
+		$args_meta = array('relation' => 'OR');
+		
+		if($keyword) {
+			$args['s'] = $keyword;
+		}
+		
+		if($cluster) {
+			$args_meta[] = array(
+				'key'     => 'primary_cluster',
+				'value'   => $cluster,
+				'compare' => 'LIKE',
+			);
+			$args_meta[] = array(
+				'key'     => 'secondary_cluster',
+				'value'   => $cluster,
+				'compare' => 'LIKE',
+			);
+		}
+		
+		if($segments) {
+			$args_meta[] = array(
+				'key'     => 'customer_segments',
+				'value'   => $segments,
+				'compare' => 'IN',
+			);
+		}
+		
+		$args['meta_query'] = $args_meta;
 		
 		$filter_query = new WP_Query($args);
 	?>
 	
-	<form method="get" id="directory-search">
+	<form method="get" id="directory-search" class="clear">
 		<div class="directory-search-row">
 			<label for="directory-search-title">Name</label>
-			<input name="post_title" id="directory-search-title" type="search">
+			<input name="keyword" id="directory-search-title" type="search" value="">
 		</div>
 		
 		<div class="directory-search-row">
+			<p><strong>Cluster</strong></p>
 			<label for="directory-search-cluster">
 				<select id="directory-search-cluster" name="cluster">
-					<?php foreach($cluster['choices'] as $option_key => $option_value) : ?>
-						<option value="<?php echo $option_value; ?>"><?php echo $option_value; ?></option>
+					<option value="">Select Cluster</option>
+					<?php foreach($cluster_field['choices'] as $option_key => $option_value) : ?>
+						<option value="<?php echo $option_value; ?>"<?php if($option_value == $cluster) {?> selected="selected"<?php } ?>><?php echo $option_value; ?></option>
 					<?php endforeach; ?>
 				</select>
 			</label>
 		</div>
 		
 		<div class="directory-search-row">
+			<p><strong>Segments</strong></p>
 			<label for="directory-search-segment">
-				<?php foreach($cluster['choices'] as $option_key => $option_value) : ?>
-					<input type="checkbox" value="<?php echo $option_value; ?>" id="directory-segment-<?php echo $option_key; ?>"><label for="directory-segment-<?php echo $option_key; ?>"><?php echo $option_value; ?></label>
+				<?php foreach($segment_field['choices'] as $option_key => $option_value) : ?>
+				
+					<input type="checkbox"name="segments[]" value="<?php echo $option_value; ?>" id="directory-segment-<?php echo $option_key; ?>"<?php if(in_array($option_value, $segments)) {?> checked="checked"<?php } ?>> <label for="directory-segment-<?php echo $option_key; ?>"><?php echo $option_value; ?></label>
 				<?php endforeach; ?>
 			</label>
 		</div>
 		
 		<div class="directory-search-row">
-			<input type="submit" class="button">
+			<a href="<?php echo get_permalink(34); ?>" class="btn btn-default read-more">Reset</a> 
+			<input type="submit" class="btn btn-default read-more">
 		</div>
 	</form>
 	
-	<div class="search-results enterprise-list">
-		
+	<div class="search-results enterprise-list" class="clear">
+		<?php if ( $filter_query->have_posts() ) : while ( $filter_query->have_posts() ) : $filter_query->the_post(); ?>
 		<article>
-			<h2><?php echo $post->post_title; ?></h2>
+			<h2><a href="<?php echo get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a></h2>
 			<?php echo get_field('description_of_business', $post->ID); ?>
 		</article>
-		
+		<?php endwhile; endif; ?>
 	</div>
 
 	<footer class="entry-footer">
