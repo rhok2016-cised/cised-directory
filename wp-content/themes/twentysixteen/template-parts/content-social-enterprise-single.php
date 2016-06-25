381cb1c38ed0e01bd066ba7d560cdecb5a46aa1e
Justin MacNeil <justin.macneil@bv02.com>
2016-06-25 13:46:51 -0400

    single social enterprise template

---
+++ wp-content/themes/twentysixteen/template-parts/content-social-enterprise-single.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * The template part for displaying single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_excerpt(); ?>
+
+	<?php twentysixteen_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php
+			the_content();
+			
+			echo get_field('description_of_business');
+		?>
+		
+		<div class="enterprise-section">
+			<h2><?php _e('Product/Service Offering', 'cised'); ?></h2>
+			<?php echo get_field('product_service_offering'); ?>
+		</div>
+		
+		<div class="enterprise-section">
+			<h2><?php _e('Contact Information', 'cised'); ?></h2>
+			<p>
+				<strong>Phone:</strong> <?php echo get_field('phone_main'); ?><br>
+				<strong>Email:</strong> <?php echo get_field('email'); ?>
+			</p>
+		</div>
+		
+		<?php
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+
+			if ( '' !== get_the_author_meta( 'description' ) ) {
+				get_template_part( 'template-parts/biography' );
+			}
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php twentysixteen_entry_meta(); ?>
+		<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
