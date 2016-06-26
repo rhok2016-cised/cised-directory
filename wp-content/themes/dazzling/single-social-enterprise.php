394eb243c1115c3d29c92ed09b577f1fcc779fed
chimo <chimo@chromic.org>
2016-06-25 23:29:07 -0400

    single-enterprise: Remove default Wordpress sidebar

--- wp-content/themes/dazzling/single-social-enterprise.php
+++ wp-content/themes/dazzling/single-social-enterprise.php
@@ -6,7 +6,7 @@
  */
 
 get_header(); ?>
-	<div id="primary" class="content-area col-sm-12 col-md-8">
+	<div id="primary" class="content-area col-sm-12 col-md-12">
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
@@ -27,5 +27,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
