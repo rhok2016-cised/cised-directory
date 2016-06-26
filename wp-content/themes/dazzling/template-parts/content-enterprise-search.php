9b1659ed4c1712347b88316177398045f66e9332
Justin MacNeil <justin.macneil@bv02.com>
2016-06-26 11:21:33 -0400

    search img thumbnail

--- wp-content/themes/dazzling/template-parts/content-enterprise-search.php
+++ wp-content/themes/dazzling/template-parts/content-enterprise-search.php
@@ -154,7 +154,7 @@
 	<div class="search-results enterprise-list" class="clear">
 		<?php if ( $filter_query->have_posts() ) : while ( $filter_query->have_posts() ) : $filter_query->the_post(); ?>
 		<article>
-			<div class="thumbnail" style="width: 250px;">
+			<div class="thumbnail" style="display: inline-block; max-width: 250px;">
 				<?php the_post_thumbnail('medium'); ?>
 			</div>
 			<h2><a href="<?php echo get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a></h2>
